dde65c68429a7d0e6db2b1dde92b7e5e93dbb321
LENOVO <LENOVO@DESKTOP-CJA315F>
2021-05-08 22:57:12 +0530

    push to commit

---
+++ Sample_Project/src/test/java/com/crm/cc/productTest/ProductInfoTest.java
@@ -0,0 +1,12 @@
+package com.crm.cc.productTest;
+
+import org.testng.annotations.Test;
+
+public class ProductInfoTest {
+	@Test
+	public void Demo2()
+	{
+		System.out.println("Welcome to Java");
+	}
+
+}
---
+++ Sample_Project/src/test/java/com/crm/cc/productTest/ProductTest.java
@@ -0,0 +1,12 @@
+package com.crm.cc.productTest;
+
+import org.testng.annotations.Test;
+
+public class ProductTest {
+	@Test
+	public void Demo() 
+	{
+		System.out.println("welcome to jenkins");
+	}
+
+}
