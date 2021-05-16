747a8cf091beb79961489f117fd4044dec182cf9
LENOVO <LENOVO@DESKTOP-CJA315F>
2021-05-16 06:01:38 +0530

    new pushed code

--- Sample_Project/src/test/java/com/crm/cc/productTest/ProductInfoTest.java
+++ Sample_Project/src/test/java/com/crm/cc/productTest/ProductInfoTest.java
@@ -3,10 +3,16 @@ package com.crm.cc.productTest;
 import org.testng.annotations.Test;
 
 public class ProductInfoTest {
-	@Test
+	@Test(groups = "regressionTest")
 	public void Demo2()
 	{
 		System.out.println("Welcome to Java and selenium");
 	}
+	
+	@Test(groups = "regressionTest")
+
+	public void createProduct() {
+		System.out.println("Product Created");
+ 	}
 
 }
--- Sample_Project/src/test/java/com/crm/cc/productTest/ProductTest.java
+++ Sample_Project/src/test/java/com/crm/cc/productTest/ProductTest.java
@@ -3,10 +3,20 @@ package com.crm.cc.productTest;
 import org.testng.annotations.Test;
 
 public class ProductTest {
-	@Test
+	@Test(groups = "smokeTest")
 	public void Demo() 
 	{
+		String BROWSER = System.getProperty("browser");
+		String ENV = System.getProperty("env");
+		System.out.println("BROWSER===========>"+BROWSER);
+		System.out.println("BROWSER===========>"+ENV);
 		System.out.println("welcome to jenkins");
 	}
+	
+	@Test(groups = "regressionTest")
+
+	public void createProductWithIDTest() {
+		System.out.println("execute createProductWithIDTest");
+ 	}
 
 }
